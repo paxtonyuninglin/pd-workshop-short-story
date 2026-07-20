4c8d06ca645ff0615fd3e9087db3b846143c5693
paxtonyuninglin <paxtonlinn@gmail.com>
2026-07-20 11:10:50 -0700

    Started the script for the short story

---
+++ short_story_script.R
@@ -0,0 +1,7 @@
+#######################################################################
+###                  Adventures of Luncinda and Abigail             ###
+###                      A Gaynor Lab short story                   ###
+#######################################################################
+
+# Once upon a time there were 2 collared antelope, Lucinda the waterbuck and Abigail the impala
+#
