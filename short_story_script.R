a5bffad3397f622563d58bdeba3e0858914d1952
martadebrina <mtogator@students.cs.ubc.ca>
2026-07-23 11:20:01 -0700

    Marta add the next line to the story

--- short_story_script.R
+++ short_story_script.R
@@ -5,4 +5,5 @@
 
 # Once upon a time there were 2 collared antelope, Lucinda the waterbuck and Abigail the impala
 # They were besties and loved eating all of the tasty corn humans were growing in a field nearby
-#But one day, a mysterious figure appeared.
+# But one day, a mysterious figure appeared.
+# The figure was carrying a huge basket filled with freshly picked corn.
